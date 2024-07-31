8672433e8b1b8866c28dba6f681c43c6d2efa79a
kellyofo <keofordile@gmail.com>
2024-07-31 12:25:08 +0100

    Update azure_openai.py

--- azure_openai.py
+++ azure_openai.py
@@ -9,7 +9,7 @@ openai.api_base = "https://apolloai.openai.azure.com/"
 openai.api_version = "2024-05-13"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def get_completion_from_messages(system_message, user_message, model="gpt-4", temperature=0, max_tokens=500) -> str:
+def get_completion_from_messages(system_message, user_message, model="gpt-4o", temperature=0, max_tokens=500) -> str:
 
     messages = [
         {'role': 'system', 'content': system_message},
